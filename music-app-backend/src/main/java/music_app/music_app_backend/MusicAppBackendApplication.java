ddc9507105709ed683a6d14b346d02fedd1390f3
Claudioflew <126118326+Claudioflew@users.noreply.github.com>
2024-09-11 14:44:53 -0700

    Created Spring Boot

---
+++ music-app-backend/src/main/java/music_app/music_app_backend/MusicAppBackendApplication.java
@@ -0,0 +1,13 @@
+package music_app.music_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicAppBackendApplication.class, args);
+	}
+
+}
---
+++ music-app-backend/src/test/java/music_app/music_app_backend/MusicAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package music_app.music_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
