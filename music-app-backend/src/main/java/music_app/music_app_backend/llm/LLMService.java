bb8efe8a0e98ece360b99a9a3fe6ef270164b661
knakatasf <knakata@dons.usfca.edu>
2024-09-14 20:12:45 -0700

    Set up application.properties for mysql and openai-api-key

---
+++ music-app-backend/src/main/java/music_app/music_app_backend/llm/LLMService.java
@@ -0,0 +1,33 @@
+package music_app.music_app_backend.llm;
+
+import music_app.music_app_backend.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.service.*;
+
+ @Service
+ public class LLMService {
+     private final AppConfig appConfig;
+
+     @Autowired
+     public LLMService(AppConfig appConfig) {
+         this.appConfig = appConfig;
+     }
+
+     interface AiRecommender {
+         @UserMessage("Recommend three songs based on the user's following favorites: {{it}}")
+         String[] recommendBasedOn(String text);
+     }
+
+     public String[] recommend(String input) {
+         ChatLanguageModel model = OpenAiChatModel.withApiKey(System.getenv("OPENAI_API_KEY"));
+         AiRecommender aiRecommender = AiServices.create(AiRecommender.class, model);
+
+         String[] recommends = aiRecommender.recommendBasedOn(input);
+         System.out.println(recommends);
+         return recommends;
+     }
+}
+
